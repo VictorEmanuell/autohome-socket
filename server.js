919aadb3ed07bc186b07cf956290bcb633ec3302
VictorEmanuell <victoremanuelexx@gmail.com>
2021-03-02 21:53:30 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const net = require('net')
+
+const handleConnection = socket => {
+    console.log("Alguém se conectou!")
+    socket.on('data', data => {
+        console.log(data.toString())
+    })
+}
+
+const server = net.createServer(handleConnection)
+
+server.listen(4000)
